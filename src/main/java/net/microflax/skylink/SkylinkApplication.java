70feb3b3cdc1f4073854a56d1e78947b96559360
Alex Tarau <alex@tarau.net>
2024-06-29 16:25:39 -0400

    Create MySQL schema

---
+++ src/main/java/net/microflax/skylink/SkylinkApplication.java
@@ -0,0 +1,13 @@
+package net.microflax.skylink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkylinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkylinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/microflax/skylink/SkylinkApplicationTests.java
@@ -0,0 +1,13 @@
+package net.microflax.skylink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkylinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
