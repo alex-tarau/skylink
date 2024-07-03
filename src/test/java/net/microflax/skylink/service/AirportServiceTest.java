fb5a816908226a5810705e4671f2dc1d36c0198f
Alex Tarau <alex@tarau.net>
2024-07-03 13:44:54 -0400

    Tweak the AirportServiceTest

---
+++ src/test/java/net/microflax/skylink/service/AirportServiceTest.java
@@ -0,0 +1,65 @@
+package net.microflax.skylink.service;
+
+import com.github.javafaker.*;
+import net.microflax.skylink.jpa.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class AirportServiceTest {
+
+
+    @Mock
+    private Faker faker;
+
+    @Mock
+    private Aviation aviation;
+
+    @Mock
+    private Address address;
+
+    private Airport airport;
+    private Location location;
+
+    @Mock
+    private AirportRepository airportRepository;
+
+    @Mock
+    private LocationRepository locationRepository;
+
+    @InjectMocks
+    private AirportService airportService;
+
+    @BeforeEach
+    void setUp(){
+        airport= new Airport();
+        location= new Location();
+        when(airportRepository.save(airport)).thenReturn(airport);
+        when(airportRepository.save(airport)).thenReturn(airport);
+        when(airportRepository.save(airport)).thenReturn(airport);
+        when(airportRepository.save(airport)).thenReturn(airport);
+        when(airportRepository.save(airport)).thenReturn(airport);
+        when(airportRepository.save(airport)).thenReturn(airport);
+        when(locationRepository.save(location)).thenReturn(location);
+        when(locationRepository.save(location)).thenReturn(location);
+        when(locationRepository.save(location)).thenReturn(location);
+        when(locationRepository.save(location)).thenReturn(location);
+        when(locationRepository.save(location)).thenReturn(location);
+        when(locationRepository.save(location)).thenReturn(location);
+    }
+
+
+    @Test
+    void generate() {
+        airportService.generate();
+        verify(airportRepository,times(6)).save(airport);
+        verify(locationRepository,times(6)).save(location);
+    }
+}
