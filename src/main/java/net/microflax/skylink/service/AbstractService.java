1bbf8d6c3e177380a788354b4f4c3a27381c7660
Alex Tarau <alex@tarau.net>
2024-07-03 13:45:39 -0400

    Add a method to persist one entity only

--- src/main/java/net/microflax/skylink/service/AbstractService.java
+++ src/main/java/net/microflax/skylink/service/AbstractService.java
@@ -5,12 +5,32 @@ import com.github.javafaker.Country;
 import com.github.javafaker.Faker;
 import org.slf4j.Logger;
 
-public abstract class AbstractService {
+public abstract class AbstractService<E> {
 
-    protected final Faker faker = new Faker();
-    protected final Aviation aviation = faker.aviation();
-    protected final Country country = faker.country();
+    private final Faker faker = new Faker();
+    private final Aviation aviation = faker.aviation();
+    private final Country country = faker.country();
     protected static final int NUMBER_OF_ENTITIES_TO_PERSIST = 6;
 
-    public abstract void persist();
+    /**
+     * Persist an entity to the database
+     */
+    public abstract void persist(E entity);
+
+    /**
+     * Generate entities to display in the UI
+     */
+    public abstract void generate();
+
+    public Faker getFaker() {
+        return faker;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public Aviation getAviation() {
+        return aviation;
+    }
 }
