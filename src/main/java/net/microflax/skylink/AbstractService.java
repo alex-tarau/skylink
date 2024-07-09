caadb9e1d86c2d696100cf2a0c1c3b1ba3ba8bfb
Alex Tarau <alex@tarau.net>
2024-07-08 21:09:25 -0400

    Remove the data faker in the AbstractService and tweak the ReservationService

--- src/main/java/net/microflax/skylink/AbstractService.java
+++ src/main/java/net/microflax/skylink/AbstractService.java
@@ -5,13 +5,10 @@ import com.github.javafaker.Country;
 import com.github.javafaker.DateAndTime;
 import com.github.javafaker.Faker;
 
+/**
+ * The base class for services
+ */
 public abstract class AbstractService {
-
-    private final Faker faker = new Faker();
-    private final Aviation aviation = faker.aviation();
-    private final Country country = faker.country();
-    private final DateAndTime dateAndTime = getFaker().date();
-    private static final int NUMBER_OF_ENTITIES_TO_PERSIST = 6;
 
 
     /**
@@ -19,19 +16,5 @@ public abstract class AbstractService {
      */
     public abstract void generate();
 
-    public Faker getFaker() {
-        return faker;
-    }
 
-    public Country getCountry() {
-        return country;
-    }
-
-    public Aviation getAviation() {
-        return aviation;
-    }
-
-    public DateAndTime getDateAndTime() {
-        return dateAndTime;
-    }
 }
--- src/main/java/net/microflax/skylink/reservation/ReservationService.java
+++ src/main/java/net/microflax/skylink/reservation/ReservationService.java
@@ -1,10 +1,13 @@
 package net.microflax.skylink.reservation;
 
 import net.microflax.skylink.AbstractService;
+import net.microflax.skylink.flight.FlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 public class ReservationService extends AbstractService {
@@ -23,5 +26,19 @@ public class ReservationService extends AbstractService {
     @Override
     public void generate() {
         reservationSimulator.run();
+    }
+
+    /**
+     * Update the number of available seats in the flight
+     *
+     * @param id the id of the reservation
+     */
+    public void updateAvailableSeats(int id) {
+        Optional<Reservation> reservation = reservationRepository.findById(id);
+        if (reservation.isEmpty()) throw new NoSuchElementException("The reservation is not in the database");
+        if (reservation.get().getFlight().getAvailableSeats() == 0) throw new UnsupportedOperationException(
+                "There are no more available seats on the flight");
+        reservation.get().getFlight().setAvailableSeats(reservation.get().getFlight().getAvailableSeats() - 1);
+        reservationRepository.save(reservation.get());
     }
 }
