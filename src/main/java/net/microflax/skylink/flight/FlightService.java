cc46ea5a85cf9d1aa8b973e74e573c206305425f
Alex Tarau <alex@tarau.net>
2024-08-02 16:07:31 -0400

    Start the flight-list and landing html pages and added the Font Awesome Icons maven dependency.

--- src/main/java/net/microflax/skylink/flight/FlightService.java
+++ src/main/java/net/microflax/skylink/flight/FlightService.java
@@ -1,10 +1,12 @@
 package net.microflax.skylink.flight;
 
 import net.microflax.skylink.AbstractService;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class FlightService extends AbstractService {
@@ -13,7 +15,7 @@ public class FlightService extends AbstractService {
     @Autowired
     private FlightRepository flightRepository;
 
-        /**
+    /**
      * Persist the flight in the database
      *
      * @param flight the flight
