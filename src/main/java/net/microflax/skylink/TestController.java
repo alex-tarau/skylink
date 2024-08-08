0f148a20b9d81b47453ad5c7e6e4f20d6b2dcd0f
Alex Tarau <alex@tarau.net>
2024-08-08 12:09:17 -0400

    Start building the layout with Ceres theme

---
+++ src/main/java/net/microflax/skylink/TestController.java
@@ -0,0 +1,13 @@
+package net.microflax.skylink;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
