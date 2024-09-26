80e6eb43ac62d8a4c69e9e91adc67d8f2b8c57fd
Alex Tarau <alex@tarau.net>
2024-09-26 11:05:08 -0400

    Add service annotation to AirplaneService

--- src/main/java/net/microflax/skylink/airplane/AirplaneService.java
+++ src/main/java/net/microflax/skylink/airplane/AirplaneService.java
@@ -2,7 +2,9 @@ package net.microflax.skylink.airplane;
 
 import net.microflax.skylink.AbstractService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AirplaneService extends AbstractService{
 
     @Autowired
