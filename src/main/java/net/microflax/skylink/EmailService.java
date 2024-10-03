002c84f12513299bc2ef3258528956a306742ed8
Alex Tarau <alex@tarau.net>
2024-10-02 20:35:26 -0400

    Refactoring the EmailService

--- src/main/java/net/microflax/skylink/EmailService.java
+++ src/main/java/net/microflax/skylink/EmailService.java
@@ -7,6 +7,7 @@ import net.microflax.skylink.payment.PaymentRepository;
 import net.microflax.skylink.reservation.Reservation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
@@ -31,26 +32,21 @@ public class EmailService {
      */
     @Transactional
     public void sendConfirmationEmail(int id) {
-        SimpleMailMessage message = new SimpleMailMessage();
         Optional<Payment> optionalPayment = paymentRepository.findById(id);
-        if (optionalPayment.isPresent()){
-            if (optionalPayment.get().getStatus() != Payment.Status.SUCCESS) throw new IllegalArgumentException("The payment was not successful");
-        }else {
+        if (optionalPayment.isPresent()) {
+            if (optionalPayment.get().getStatus() != Payment.Status.SUCCESS)
+                throw new IllegalArgumentException("The payment was not successful");
+        } else {
             throw new NoSuchElementException("The payment is not persisted in the database");
         }
-        Reservation reservation = optionalPayment.get().getReservation();
-        Flight flight = reservation.getFlight();
-        message.setSubject("Booking confirmation for " + flight.getOriginAirport().getCity() + " " +
-                flight.getDestinationAirport().getCity());
-        message.setFrom("smtp.gmail.com");
-        Passenger passenger = reservation.getPassenger();
-        message.setTo(passenger.getEmail());
-        message.setSentDate(new Date());
-        Payment payment = optionalPayment.get();
-        String body = createBody(passenger, flight, payment);
-        message.setText(body);
-        mailSender.send(message);
+        SimpleMailMessage message = createConfirmationEmail(optionalPayment.get());
+        try {
+            mailSender.send(message);
+        } catch (MailException e) {
+            //
+        }
     }
+
 
     @Bean
     public JavaMailSender getJavaMailSender() {
@@ -67,19 +63,34 @@ public class EmailService {
         return mailSender;
     }
 
-    private String createBody(Passenger passenger, Flight flight, Payment payment) {
+    private SimpleMailMessage createConfirmationEmail(Payment payment) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        Reservation reservation = payment.getReservation();
+        Flight flight = reservation.getFlight();
+        message.setSubject("Booking confirmation for " + flight.getOriginAirport().getCity() + " - " +
+                flight.getDestinationAirport().getCity());
+        message.setFrom("smtp.gmail.com");
+        Passenger passenger = reservation.getPassenger();
+        message.setTo(passenger.getEmail());
+        message.setSentDate(new Date());
+        String body = createConfirmationEmailBody(passenger, flight, payment);
+        message.setText(body);
+        return message;
+    }
+
+    private String createConfirmationEmailBody(Passenger passenger, Flight flight, Payment payment) {
         Set<Passenger> dependents = passenger.getDependents();
         String passengerDependents = "";
         if (passenger.getDependents() == null) {
             passengerDependents = "";
-        }else {
-            for (Passenger dependent: dependents){
-                passengerDependents += (dependent.getFirstName() + " "+ dependent.getLastName());
+        } else {
+            for (Passenger dependent : dependents) {
+                passengerDependents += (dependent.getFirstName() + " " + dependent.getLastName());
             }
         }
         return new StringBuilder().append("Flight confirmed").append("\n\n\n").append("Hi ").append(passenger.getFirstName()).
                 append("\n").append("Your ").append(flight.getOriginAirport().getCity()).append(" - ").
-                append(flight.getDestinationAirport().getCity() + " ").append("flight is confirmed").append("\n").
+                append(flight.getDestinationAirport().getCity()).append(" ").append("flight is confirmed").append("\n").
                 append("We will email you your ticket shortly").append("\n\n\n\n\n").append("Just Booked" + "\n").
                 append(flight.getAirline().getName()).append(" ").append(flight.getName()).append("\n").
                 append(flight.getOriginAirport().getCity()).append(" -> ").append(flight.getDestinationAirport().getCity())
@@ -89,4 +100,5 @@ public class EmailService {
                 append(payment.getMethod().name()).append("\n Credit Card \n").append(payment.getCreditCardNumber()).
                 append("\n ").append("Total Charge \n").append(payment.getAmount()).toString();
     }
+
 }
